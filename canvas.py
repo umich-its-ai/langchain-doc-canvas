2b1f9cd03af402d320c9e2373380036926850e3f
John Pariseau <simplymail@ururk.com>
2023-07-25 17:26:05 -0400

    Code cleanup

--- canvas.py
+++ canvas.py
@@ -12,12 +12,6 @@ from langchain.document_loaders.excel import UnstructuredExcelLoader
 from striprtf.striprtf import rtf_to_text
 from langchain.document_loaders.markdown import UnstructuredMarkdownLoader
 
-# Import the html parser class
-from bs4 import BeautifulSoup
-
-# Import PDF parser class
-from PyPDF2 import PdfReader
-
 class CanvasLoader(BaseLoader):
     """Loading logic for Canvas Pages, Announcements, Assignments and Files."""
 
@@ -45,7 +39,7 @@ class CanvasLoader(BaseLoader):
             )
 
             for page in pages:
-                page_body_text = BeautifulSoup(page.body, "lxml").text
+                page_body_text = self._get_html_as_string(page.body)
 
                 page_documents.append(Document(
                     page_content=page_body_text.strip(),
@@ -69,10 +63,10 @@ class CanvasLoader(BaseLoader):
             )
 
             for announcement in announcements:
-                page_body_text = BeautifulSoup(announcement.message, "lxml").text
+                page_body_text = self._get_html_as_string(announcement.message)
 
                 announcement_documents.append(Document(
-                    page_content=page_body_text.strip(),
+                    page_content=page_body_text,
                     metadata={ "title": announcement.title, "kind": "announcement", "announcement_id": announcement.id }
                 ))
         except CanvasException as e:
@@ -90,7 +84,7 @@ class CanvasLoader(BaseLoader):
 
             for assignment in assignments:
                 if assignment.description:
-                    assignment_description = BeautifulSoup(assignment.description, "lxml").text.strip()
+                    assignment_description = self._get_html_as_string(assignment.description)
                     assignment_description = f" Assignment Description: {assignment_description}\n\n"
                 else:
                     assignment_description = ""
@@ -105,6 +99,113 @@ class CanvasLoader(BaseLoader):
             print(e)
 
         return assignment_documents
+
+    def _get_html_as_string(self, html) -> str:
+
+        try:
+            # Import the html parser class
+            from bs4 import BeautifulSoup
+        except ImportError:
+            raise ImportError(
+                "Could not import beautifulsoup4 python package. "
+                "Please install it with `pip install beautifulsoup4`."
+            )
+
+        html_string = BeautifulSoup(html, "lxml").text.strip()
+
+        return html_string
+
+    def _load_text_file(self, file) -> List[Document]:
+        file_contents = file.get_contents(binary=False)
+
+        return [Document(
+            page_content=file_contents.strip(),
+            metadata={ "filename": file.filename, "kind": "file", "file_id": file.id }
+        )]
+
+    def _load_html_file(self, file) -> List[Document]:
+        file_contents = file.get_contents(binary=False)
+
+        return [Document(
+            page_content=self._get_html_as_string(file_contents),
+            metadata={ "filename": file.filename, "kind": "file", "file_id": file.id }
+        )]
+
+    def _load_rtf_file(self, file) -> List[Document]:
+        file_contents = file.get_contents(binary=False)
+
+        return[Document(
+            page_content=rtf_to_text(file_contents).strip(),
+            metadata={ "filename": file.filename, "kind": "file", "file_id": file.id }
+        )]
+
+    def _load_pdf_file(self, file) -> List[Document]:
+        try:
+            # Import PDF parser class
+            from PyPDF2 import PdfReader
+        except ImportError:
+            raise ImportError(
+                "Could not import PyPDF2 python package. "
+                "Please install it with `pip install PyPDF2`."
+            )
+
+        file_contents = file.get_contents(binary=True)
+        pdf_reader = PdfReader(BytesIO(file_contents))
+
+        docs = []
+
+        for i, page in enumerate(pdf_reader.pages):
+            docs.append(Document(
+                page_content=page.extract_text(),
+                metadata={ "filename": file.filename, "kind": "file", "file_id": file.id, "page": i }
+            ))
+
+        return docs
+
+    def _load_docx_file(self, file) -> List[Document]:
+        file_contents = file.get_contents(binary=True)
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            file_path = f"{temp_dir}/{file.filename}"
+
+            with open(file_path, "wb") as binary_file:
+                # Write bytes to file
+                binary_file.write(file_contents)
+
+            loader = Docx2txtLoader(file_path)
+            docs = loader.load()
+
+        return docs
+
+    def _load_excel_file(self, file) -> List[Document]:
+        file_contents = file.get_contents(binary=True)
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            file_path = f"{temp_dir}/{file.filename}"
+
+            with open(file_path, "wb") as binary_file:
+                # Write bytes to file
+                binary_file.write(file_contents)
+
+            loader = UnstructuredExcelLoader(file_path)
+            docs = loader.load()
+
+        return docs
+
+    def _load_md_file(self, file) -> List[Document]:
+        file_contents = file.get_contents(binary=True)
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            file_path = f"{temp_dir}/{file.filename}"
+
+            with open(file_path, "wb") as binary_file:
+                # Write bytes to file
+                binary_file.write(file_contents)
+
+            loader = UnstructuredMarkdownLoader(file_path)
+            docs = loader.load()
+
+        return docs
 
     def load_files(self, course) -> List[Document]:
         from canvasapi.exceptions import CanvasException
@@ -135,89 +236,25 @@ class CanvasLoader(BaseLoader):
                     # print(f"Processing {file.filename} {file.mime_class}")
 
                     if file_content_type == "text/plain":
-                        file_contents = file.get_contents(binary=False)
-
-                        file_documents.append(Document(
-                            page_content=file_contents.strip(),
-                            metadata={ "filename": file.filename, "kind": "file", "file_id": file.id }
-                        ))
+                        file_documents = file_documents + self._load_text_file(file)
 
                     if file_content_type == "text/html":
-                        file_contents = file.get_contents(binary=False)
-
-                        page_body_text = BeautifulSoup(file_contents, "lxml").text
-
-                        file_documents.append(Document(
-                            page_content=page_body_text.strip(),
-                            metadata={ "filename": file.filename, "kind": "file", "file_id": file.id }
-                        ))
+                        file_documents = file_documents + self._load_html_file(file)
 
                     elif file_content_type == "application/pdf":
-                        file_contents = file.get_contents(binary=True)
-                        pdf_reader = PdfReader(BytesIO(file_contents))
-
-                        for i, page in enumerate(pdf_reader.pages):
-                            file_documents.append(Document(
-                                page_content=page.extract_text(),
-                                metadata={ "filename": file.filename, "kind": "file", "file_id": file.id, "page": i }
-                            ))
+                        file_documents = file_documents + self._load_pdf_file(file)
 
                     elif file_content_type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-                        file_contents = file.get_contents(binary=True)
-
-                        with tempfile.TemporaryDirectory() as temp_dir:
-                            file_path = f"{temp_dir}/{file.filename}"
-
-                            with open(file_path, "wb") as binary_file:
-                                # Write bytes to file
-                                binary_file.write(file_contents)
-
-                            loader = Docx2txtLoader(file_path)
-                            docs = loader.load()
-
-                            file_documents = file_documents + docs
+                        file_documents = file_documents + self._load_docx_file(file)
 
                     elif file_content_type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" or file_content_type == "application/vnd.ms-excel":
-                        file_contents = file.get_contents(binary=True)
-
-                        with open(f"/tmp/{file.filename}", "wb") as binary_file:
-                            # Write bytes to file
-                            binary_file.write(file_contents)
-
-                        with tempfile.TemporaryDirectory() as temp_dir:
-                            file_path = f"{temp_dir}/{file.filename}"
-
-                            with open(file_path, "wb") as binary_file:
-                                # Write bytes to file
-                                binary_file.write(file_contents)
-
-                            loader = UnstructuredExcelLoader(file_path)
-                            docs = loader.load()
-
-                            file_documents = file_documents + docs
+                        file_documents = file_documents + self._load_excel_file(file)
 
                     elif file_content_type == "text/markdown":
-                        file_contents = file.get_contents(binary=True)
-
-                        with tempfile.TemporaryDirectory() as temp_dir:
-                            file_path = f"{temp_dir}/{file.filename}"
-
-                            with open(file_path, "wb") as binary_file:
-                                # Write bytes to file
-                                binary_file.write(file_contents)
-
-                            loader = UnstructuredMarkdownLoader(file_path)
-                            docs = loader.load()
-
-                            file_documents = file_documents + docs
+                        file_documents = file_documents + self._load_md_file(file)
 
                     elif file_content_type == "text/rtf":
-                        file_contents = file.get_contents(binary=False)
-
-                        file_documents.append(Document(
-                            page_content=rtf_to_text(file_contents).strip(),
-                            metadata={ "filename": file.filename, "kind": "file", "file_id": file.id }
-                        ))
+                        file_documents = file_documents + self._load_rtf_file(file)
                 else:
                     self.invalid_files.append(f"{file.filename} ({file_content_type})")
         except CanvasException as e:
@@ -244,7 +281,7 @@ class CanvasLoader(BaseLoader):
             course = canvas.get_course(self.course_id)
 
             # Access the course's name
-            print("Indexing: " + course.name)
+            print(f"Indexing: {course.name}")
             print("")
 
             # Load pages
