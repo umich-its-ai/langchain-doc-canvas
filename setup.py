5c21c6cf097a3a294498f29605557767bd40cfd9
mlhess <mlhess@umich.edu>
2023-07-29 14:16:25 -0400

    create a package

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Canvas langchain integration',
+    version='0.1',
+    description='A canvas langchain integration',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='University of Michigan',
+    author_email='noreply@umich.edu',
+    url='https://github.com/umich-its-ai/langchain-doc-canvas',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+    ],
+    install_requires=[
+        'langchain',
+        'unstructured',
+        'canvasapi',
+        'beautifulsoup4',
+        'lxml',
+        'PyPDF2',
+        'striprtf'
+    ],
+)
