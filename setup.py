522f441bd928018509ed9e9b268e73a9d50d133c
John Pariseau <simplymail@ururk.com>
2023-08-11 14:32:44 -0400

    Include additional requirements for xls and md parsing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='canvas_langchain',
-    version='0.2',
+    version='0.3',
     description='A canvas langchain integration',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -22,7 +22,11 @@ setup(
         'lxml',
         'PyPDF2',
         'docx2txt',
-        'striprtf'
+        'striprtf',
+        'pandas',
+        'openpyxl',
+        'xlrd',
+        'markdown'
     ],
     python_requires='>=3.8.1',
 )
